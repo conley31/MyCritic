294fc60bcfe57c23909ed75b28b4c861fd674962
U-BRANDON-ASUS\Brandon <kent13@purdue.edu>
2018-02-08 00:36:46 -0500

    basic query to get movies currently in theaters

---
+++ public/movie.js
@@ -0,0 +1,4 @@
+//get movies currently in theaters
+$.getJSON('https://api.themoviedb.org/3/discover/movie?api_key=d26e26ba96250fb462f04e8c480e3351&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=2018-01-15&primary_release_date.lte=2018-02-08', function(json){
+	
+});
