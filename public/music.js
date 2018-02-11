1785ba11df3e812ea72a6b7d3f50368409b2ec5e
JacobConley <conley31@purdue.edu>
2018-02-10 19:59:39 -0500

    changed song structure to rely on id instead of name so its more acurate

--- public/music.js
+++ public/music.js
@@ -1,12 +1,37 @@
+//GET Spotify api Key
+var spotifyApi = new SpotifyWebApi();
 
-//get the top 50 songs and display them
-$.getJSON('https://itunes.apple.com/us/rss/topsongs/limit=50/json', function(json){
-	var bodyDiv = document.getElementById("songList");
-	var html = "";
-	for( i = 0; i < 50; i++){
-		html += "<div style=\"margin-left: 10%; border-bottom-style: solid; border-width: 2px\"; onclick=\"window.location=\'/song/" + json["feed"]["entry"][i]["im:name"]["label"] + "\'\"'><h1>" + json["feed"]["entry"][i]["im:name"]["label"] + "</h1></div>";
-	}
-	bodyDiv.innerHTML = html;
-});
+var request = new XMLHttpRequest();
+request.open('GET', "/access");
+request.responseType = 'text';
+var token = "";
+request.onload = function() {
+  token = request.response;
+  songList();
+};
+
+request.send();
+var bodyDiv = document.getElementById("songList");
+var html = "";
+
+var songList = function(){
+	spotifyApi.setAccessToken(token);
+	//get the top 50 songs and display them
+	$.getJSON('https://itunes.apple.com/us/rss/topsongs/limit=50/json', async function(json){
+		for( i = 0; i < 50; i++){
+			var song = json["feed"]["entry"][i]["im:name"]["label"];
+			var tempSong = song;
+			if(song.indexOf("(feat.") != -1){
+				tempSong = song.slice(0, song.indexOf("(feat."))
+			}
+			console.log(json["feed"]["entry"][i]["im:artist"]["label"]);
+			await spotifyApi.searchTracks(tempSong, {limit: 5, artist: json["feed"]["entry"][i]["im:artist"]["label"]}).then(function(data) {
+				console.log(data["tracks"]["items"][0]["name"]);
+				html += "<div style=\"margin-left: 10%; border-bottom-style: solid; border-width: 2px\"; onclick=\"window.location=\'/song/" + data["tracks"]["items"][0]["id"] + "\'\"'><h1>" + data["tracks"]["items"][0]["name"] + "</h1></div>";
+			});
+		}
+		bodyDiv.innerHTML = html;
+	});
+}
 
 
