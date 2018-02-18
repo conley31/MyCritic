4efef4eb1af09b4eae580aaf480eafd602a8ff5f
Jordan Hagedorn <hagedorj@purdue.edu>
2018-02-18 10:58:00 -0500

    add ID field for each div, fix music regex, add game tests

--- public/gameTitle.js
+++ public/gameTitle.js
@@ -6,7 +6,7 @@ request.onload = function(){
     gameObj = request.response;
     var html = "<h1 style=\"margin-left: 10%; margin-right: 10%\">" + gameObj[0].name + "</h1>"; 
     html += "<h3 style=\"margin-left: 10%\">" + gameObj[0].summary + "</h3>";
-    html += "<h5 style=\"margin-left: 10%\">Total Rating: " + gameObj[0].total_rating + "%</h5>";
+    html += "<h5 id=\"ratingNumber\" style=\"margin-left: 10%\">Total Rating: " + gameObj[0].total_rating + "%</h5>";
     bodyDiv.innerHTML = html;
 };
 
