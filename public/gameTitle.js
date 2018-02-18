bcad714da5b4dc400569938dabd48124668c22ec
Jordan Hagedorn <hagedorj@purdue.edu>
2018-02-18 11:35:52 -0500

    fix for Media Summary Page defect 1

--- public/gameTitle.js
+++ public/gameTitle.js
@@ -4,9 +4,14 @@ request.responseType = 'json';
 request.onload = function(){
     var bodyDiv = document.getElementById('gameInfo');
     gameObj = request.response;
+    var rating = gameObj[0].total_rating;
+    if(rating == undefined){
+        rating = "This game currently has no ratings!"
+    }
+    else rating += '%';
     var html = "<h1 style=\"margin-left: 10%; margin-right: 10%\">" + gameObj[0].name + "</h1>"; 
     html += "<h3 style=\"margin-left: 10%\">" + gameObj[0].summary + "</h3>";
-    html += "<h5 id=\"ratingNumber\" style=\"margin-left: 10%\">Total Rating: " + gameObj[0].total_rating + "%</h5>";
+    html += "<h5 id=\"ratingNumber\" style=\"margin-left: 10%\">Total Rating: " + rating + "</h5>";
     bodyDiv.innerHTML = html;
 };
 
