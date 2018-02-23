8c6fb78994b47af1d39fce9b98acfa814ca473e7
xhpwn <saxena20@purdue.edu>
2018-02-23 17:59:45 -0500

    Movies + Movieslist UI fully done

--- public/movies.js
+++ public/movies.js
@@ -1,12 +1,21 @@
 //get movies currently in theaters
 $.getJSON('https://api.themoviedb.org/3/discover/movie?api_key=d26e26ba96250fb462f04e8c480e3351&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=2018-01-15&primary_release_date.lte=2018-02-08', function(json){
 	var bodyDiv = document.getElementById("movieList");
-	console.log(bodyDiv);
+console.log(bodyDiv);
 	var html = "";
 	console.log(json);
 	for ( i = 0; i < 20; i++) {
-		html += "<div style=\"margin-left: 10%; border-bottom-style: solid; border-width: 2px\"; onclick=\"window.location=\'/movie/" + json["results"][i]["id"] + "\'\"'><h1>" + json["results"][i]["title"] + "</h1></div>";
+
+		html += "<div onmouseout=\"this.style.color=\'black\'\" onmouseover=\"this.style.color=\'#4b6d93\'\" style=\"margin-left: 25%; margin-bottom: 2%; width: 50%; background-color: \'white\';\"; onclick=\"window.location=\'/movie/" 
+
+        html += json["results"][i]["id"] 
+
+        html += "\'\"'> <img height=\"50px\" src=\"./staticImages/movieIcon.jpg\" align=\"right\"><h3 style=\"font-family: Arial\">" 
+
+        html += json["results"][i]["title"] + "</h3> <font color=\"#dd4300\"> Average Score</font> : 6.2</font> </div>";
+
 	}
+
 	console.log(html);
 	bodyDiv.innerHTML = html;
 	
