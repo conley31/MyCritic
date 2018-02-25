ea723e2a09f2ac09d5c24166a5331724a0c2fe95
xhpwn <saxena20@purdue.edu>
2018-02-25 17:53:57 -0500

    Almost done with game UI

--- public/gameTitle.js
+++ public/gameTitle.js
@@ -2,13 +2,24 @@ var request = new XMLHttpRequest();
 request.open('GET', "/getGame/");
 request.responseType = 'json';
 request.onload = function(){
+
     var bodyDiv = document.getElementById('gameInfo');
+
     gameObj = request.response;
-    var html = "<h1 style=\"margin-left: 10%; margin-right: 10%\">" + gameObj[0].name + "</h1>"; 
-    html += "<h3 style=\"margin-left: 10%\">" + gameObj[0].summary + "</h3>";
+
+    console.log(gameObj);
+
+    var html = "<h1 style=\"font-size: 3.5em; text-align: center; margin-top: 3%\">" + gameObj[0].name + "</h1>";
+
+    html += "<div width=\"60%\" style=\"margin-top: 5%; margin-left: 24%\">";
+
+    html += "<h5 style=\"font-family: Arial; font-size: 1.2em; width: 70%; margin-bottom: 4%;\">" + gameObj[0].summary + "</h5>";
+
     html += "<h5 style=\"margin-left: 10%\">Total Rating: " + gameObj[0].total_rating + "%</h5>";
+
+    html += "</div>";
+
     bodyDiv.innerHTML = html;
 };
 
 request.send();
-
