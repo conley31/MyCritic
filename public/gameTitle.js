9396253dcaac08f1abd57f0c8ba3ce4de65bbc70
JacobConley <conley31@purdue.edu>
2018-03-03 12:49:55 -0500

    added username

--- public/gameTitle.js
+++ public/gameTitle.js
@@ -54,7 +54,7 @@ var reviewsfunc = function(){
         //add back for the type src=\"./staticImages/movieIcon.jpg\"
         html += "<img height=\"50px\" align=\"right\"><h3 style=\"font-family: Arial\">" 
 
-        html += reviews[i]["reviewTxt"] + "</h3> <font color=\"#dd4300\"> rating </font> : " + reviews[i]["rating"] + "</font> </div>";
+        html += reviews[i]["reviewTxt"] + "</h3> <span><font color=\"#dd4300\"> rating </font> : " + reviews[i]["rating"] + "</span><span style=\"margin-left:75%\"><font color=\"#dd4300\">user</font>: "+reviews[i]["username"]+"</span></font> </div>";
     }
 
     bodyDiv.innerHTML = html;
