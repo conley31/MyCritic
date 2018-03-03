7208b7a97cbd3db8e0a9e86b6afaa89e40d418f5
JacobConley <conley31@purdue.edu>
2018-03-03 12:55:31 -0500

    title to review on user page

--- public/users.js
+++ public/users.js
@@ -22,7 +22,7 @@ var bodyDiv = document.getElementById("userReviewList");
 var html = "";
 
 var reviewList = function(){
-	html += "<h1 style=\"margin-left: 25%; margin-bottom 2%; 50%; background-color: \'white\';\";>" + name + "\'s Reviews</h1>";
+	html += "<h1 style=\"margin-left: 20%; margin-bottom 2%; 50%; background-color: \'white\';\";>" + name + "\'s Reviews</h1>";
 	var display = 0;
 	if(token.length > 20){
 		display = 20;
@@ -35,7 +35,7 @@ var reviewList = function(){
 		html += "<div id:\"" + i + "\" style=\"margin-left: 25%; margin-bottom: 2%; width: 50%; background-color: \'white\';\";>" 
 
 		//add back for the type src=\"./staticImages/movieIcon.jpg\"
-        html += "<img height=\"50px\" align=\"right\"><h3 style=\"font-family: Arial\">" 
+        html += "<img height=\"50px\" align=\"right\"><h2 style=\"font-family: Arial\">"+token[i]["title"]+"</h2><h3 style=\"font-family: Arial\">" 
 
         html += token[i]["reviewTxt"] + "</h3> <font color=\"#dd4300\"> rating </font> : " + token[i]["rating"] + "</font> </div>";
 	}
