769ef37c7fc98f1689b1410007e46aa2f7546b43
xhpwn <saxena20@purdue.edu>
2018-03-04 15:52:35 -0500

    Added loading status, error throw to be added

--- public/music.js
+++ public/music.js
@@ -12,6 +12,7 @@ request.onload = function() {
 
 request.send();
 var bodyDiv = document.getElementById("songList");
+var loadingDiv = document.getElementById("loadingDiv");
 var html = "";
 
 var songList = function(){
@@ -19,20 +20,21 @@ var songList = function(){
 	//get the top 50 songs and display them
 	$.getJSON('https://itunes.apple.com/us/rss/topsongs/limit=25/json', async function(json){
 		for( i = 0; i < 25; i++){
-			console.log(json);
 			var song = json["feed"]["entry"][i]["im:name"]["label"];
 			var tempSong = song;
 			if(song.indexOf("(feat.") != -1){
 				tempSong = song.slice(0, song.indexOf("(feat."))
 			}
 			console.log(tempSong);
+            loadingDiv.innerHTML = "<center>Loading...</center>";
 			await spotifyApi.searchTracks(tempSong, {limit: 1, artist: json["feed"]["entry"][i]["im:artist"]["label"]}).then(function(data) {
-				html += "<div onmouseout=\"this.style.color=\'black\'\" onmouseover=\"this.style.color=\'#4b6d93\'\" style=\"margin-left: 25%; margin-bottom: 2%; width: 50%; background-color: \'white\';\"; onclick=\"window.location=\'/song/" 
-       			html += data["tracks"]["items"][0]["id"] 
-        		html += "\'\"'> <img height=\"50px\" src=\"./staticImages/musicIcon.jpg\" align=\"right\"><h3 style=\"font-family: Arial\">" 
+				html += "<div onmouseout=\"this.style.color=\'black\'\" onmouseover=\"this.style.color=\'#4b6d93\'\" style=\"margin-left: 25%; margin-bottom: 2%; width: 50%; background-color: \'white\';\"; onclick=\"window.location=\'/song/"
+       			html += data["tracks"]["items"][0]["id"]
+        		html += "\'\"'> <img height=\"50px\" src=\"./staticImages/musicIcon.jpg\" align=\"right\"><h3 style=\"font-family: Arial\">"
         		html += data["tracks"]["items"][0]["name"] + "</h3> <font color=\"#dd4300\"> Average Score</font> : "+data["tracks"]["items"][0]["popularity"]+"</font> </div>";
 			});
 		}
+        loadingDiv.style.visibility = "hidden";
 		bodyDiv.innerHTML = html;
 	});
 }
