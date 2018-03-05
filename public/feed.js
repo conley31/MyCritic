34f9c3146449a0def2b4cd78bb811b4448b06baa
U-BRANDON-ASUS\Brandon <kent13@purdue.edu>
2018-03-04 21:15:26 -0500

    feed fix

--- public/feed.js
+++ public/feed.js
@@ -12,7 +12,7 @@ var bodyDiv = document.getElementById("feed-list");
 var html = "";
 
 var feedList = function(){
-	for (i = 0; i < 20; i++) {
+	for (i = 0; i < 20 && i < token.length; i++) {
 		html += "<div id:\"" + i + "\" style=\"margin-left: 25%; margin-bottom: 2%; width: 50%; background-color: \'white\';\";>" 
 
         	html += "<img height=\"50px\" align=\"right\"><h2 style=\"font-family: Arial\">"+token[i]["title"]+" ("+token[i]["type"]+")"+"</h2><h3 style=\"font-family: Arial\">" 
