aaef66d50b1355c429158ef0b14ae41d7a27a3e8
hagedorj <hagedorj@purdue.edu>
2018-03-07 00:55:43 -0500

    add image to books

--- public/books.js
+++ public/books.js
@@ -10,68 +10,28 @@ request.send();
 
 var populateHtml = function(){
     var bodyDiv = document.getElementById("bookList");
+    var loadingDiv =  document.getElementById("loadingDiv");
     console.log(bookObj[0]["GoodreadsResponse"]);
     var html = "";
     console.log(bookObj);
     for(var i = 0; i < 20; i++){
         if(bookObj[i]["GoodreadsResponse"] != undefined){
             if(bookObj[i]["GoodreadsResponse"]["book"]["title"]["_text"] == null){
-            html += "<div id=\"" + i + "\" style=\"margin-left: 10%; border-bottom-style: solid; border-width: 2px\"; onclick=\"window.location=\'/bookInfo/" + bookObj[i]["GoodreadsResponse"]["book"]["id"]["_text"] + "\'\"'><h1>" + bookObj[i]["GoodreadsResponse"]["book"]["title"]["_cdata"] + "</h1></div>";
+                html += "<div onmouseout=\"this.style.color=\'black\'\" onmouseover=\"this.style.color=\'#4b6d93\'\" style=\"margin-left: 25%; margin-bottom: 2%; width: 50%; background-color: \'white\';\"; onclick=\"window.location=\'/bookInfo/"
+                html += bookObj[i]["GoodreadsResponse"]["book"]["id"]["_text"]
+                html += "\'\"'> <img height=\"50px\" src=\"./staticImages/bookIcon.png\" align=\"right\"><h3 style=\"font-family: Arial\">"
+                html += bookObj[i]["GoodreadsResponse"]["book"]["title"]["_cdata"] + "</h3> <font color=\"#dd4300\"> Average Score</font> : "+bookObj[i]["GoodreadsResponse"]["book"]["average_rating"]["_text"]+"</font> </div>";
             }
             else{
-                html += "<div id=\"" + i + "\" style=\"margin-left: 10%; border-bottom-style: solid; border-width: 2px\"; onclick=\"window.location=\'/bookInfo/" + bookObj[i]["GoodreadsResponse"]["book"]["id"]["_text"] + "\'\"'><h1>" + bookObj[i]["GoodreadsResponse"]["book"]["title"]["_text"] + "</h1></div>";			  			
+                html += "<div onmouseout=\"this.style.color=\'black\'\" onmouseover=\"this.style.color=\'#4b6d93\'\" style=\"margin-left: 25%; margin-bottom: 2%; width: 50%; background-color: \'white\';\"; onclick=\"window.location=\'/bookInfo/"
+                html += bookObj[i]["GoodreadsResponse"]["book"]["id"]["_text"]
+                html += "\'\"'> <img height=\"50px\" src=\"./staticImages/bookIcon.png\" align=\"right\"><h3 style=\"font-family: Arial\">"
+                html += bookObj[i]["GoodreadsResponse"]["book"]["title"]["_text"] + "</h3> <font color=\"#dd4300\"> Average Score</font> : "+bookObj[i]["GoodreadsResponse"]["book"]["average_rating"]["_text"]+"</font> </div>";
 	  
             }
         }
 	
 	}
+    loadingDiv.style.visibility = "hidden";
     bodyDiv.innerHTML = html;
 }
-
-/*
-var API = "2e30dfd30ca7408a957ee54dc4aa2bc3";
-var url = "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json";
-url += '?' + $.param({
-  'api-key': API
-});
-
-var bookID = [];
-
-var bodyDiv = document.getElementById("bookList");
-var html = "";
-
-$.getJSON(url, function(json){
-	for( i = 0; i < 20; i++){
-		(async function (i) {
-			bookID[i] = new XMLHttpRequest();
-			bookID[i].responseType = 'json';
-			console.log("/getBookID?author="+json["results"][i]["author"]+"&title="+json["results"][i]["title"]);
-			var title = "";
-			if(json["results"][i]["title"].indexOf("#") != -1){
-				title = "\\"+json["results"][i]["title"];
-			}
-			else{
-				title = json["results"][i]["title"];
-			}
-			bookID[i].open('GET', "/getBookID?author="+json["results"][i]["author"]+"&title="+json["results"][i]["title"], true);
-			bookID[i].onreadystatechange = await function() {
-				if(bookID[i].response != null){
-					console.log(bookID[i].response["GoodreadsResponse"]["book"]["id"]);
-					if(bookID[i].response["GoodreadsResponse"]["book"]["title"]["_text"] == null){
-						html += "<div id=\"" + i + "\" style=\"margin-left: 10%; border-bottom-style: solid; border-width: 2px\"; onclick=\"window.location=\'/bookInfo/" + bookID[i].response["GoodreadsResponse"]["book"]["id"]["_text"] + "\'\"'><h1>" + bookID[i].response["GoodreadsResponse"]["book"]["title"]["_cdata"] + "</h1></div>";
-			  		}
-			  		else {
-						html += "<div id=\"" + i + "\" style=\"margin-left: 10%; border-bottom-style: solid; border-width: 2px\"; onclick=\"window.location=\'/bookInfo/" + bookID[i].response["GoodreadsResponse"]["book"]["id"]["_text"] + "\'\"'><h1>" + bookID[i].response["GoodreadsResponse"]["book"]["title"]["_text"] + "</h1></div>";			  			
-			  		}	
-			  		bodyDiv.innerHTML = html;
-			  	}
-			};
-			//html += "<div style=\"margin-left: 10%; border-bottom-style: solid; border-width: 2px\"; onclick=\"window.location=\'/bookInfo/" + json["results"][i]["title"] + "\'\"'><h1>" + json["results"][i]["title"] + "</h1></div>";
-			bookID[i].send();
-		})(i);
-	}
-	console.log("SHOULD BE HERE")
-});
-
-*/
-
