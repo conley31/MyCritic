71514fee1a489a54dc3ffb8d70f0ff94aea8d040
JacobConley <conley31@purdue.edu>
2018-03-11 08:53:08 -0500

    added no results underneath a media type on seach if there are no results

--- public/searchResults.js
+++ public/searchResults.js
@@ -42,41 +42,55 @@ var populateHtml = function(){
    
     //list songs
 	//
+    console.log(movies);
     html += "<div style=\"margin-left: 5%\"><h1>Songs</h1></div>";
-	if (songs["tracks"] != null) {
-    for(i = 0; i < songs["tracks"]["items"].length && i < 5; i++){
-        //html += "<div style=\"margin-left: 10%; border-bottom-style: solid; border-width: 2px\"; onclick=\"window.location=\'/song/" + songs["tracks"]["items"][i]["id"] + "\'\"'><h1>" + songs["tracks"]["items"][i]["name"] + "</h1></div>";     
-        html += "<div onmouseout=\"this.style.color=\'black\'\" onmouseover=\"this.style.color=\'#4b6d93\'\" style=\"margin-left: 25%; margin-bottom: 2%; width: 50%; background-color: \'white\';\"; onclick=\"window.location=\'/song/" 
-        html += songs["tracks"]["items"][i]["id"] 
-        html += "\'\"'> <img height=\"50px\" src=\"./staticImages/musicIcon.jpg\" align=\"right\"><h3 style=\"font-family: Arial\">" 
-        html += songs["tracks"]["items"][i]["name"] + "</h3> <font color=\"#dd4300\"> Average Score</font> : "+ songs["tracks"]["items"][i]["popularity"]+"</font> </div>";
-    }
+	if (songs["tracks"] != null && songs["tracks"]["items"].length != 0) {
+        for(i = 0; i < songs["tracks"]["items"].length && i < 5; i++){
+            //html += "<div style=\"margin-left: 10%; border-bottom-style: solid; border-width: 2px\"; onclick=\"window.location=\'/song/" + songs["tracks"]["items"][i]["id"] + "\'\"'><h1>" + songs["tracks"]["items"][i]["name"] + "</h1></div>";     
+            html += "<div onmouseout=\"this.style.color=\'black\'\" onmouseover=\"this.style.color=\'#4b6d93\'\" style=\"margin-left: 25%; margin-bottom: 2%; width: 50%; background-color: \'white\';\"; onclick=\"window.location=\'/song/" 
+            html += songs["tracks"]["items"][i]["id"] 
+            html += "\'\"'> <img height=\"50px\" src=\"./staticImages/musicIcon.jpg\" align=\"right\"><h3 style=\"font-family: Arial\">" 
+            html += songs["tracks"]["items"][i]["name"] + "</h3> <font color=\"#dd4300\"> Average Score</font> : "+ songs["tracks"]["items"][i]["popularity"]+"</font> </div>";
+        }
 	}
+    else {
+        html += "<div style=\"margin-left: 25%; margin-top: 2%\"><h2>No Results</h2></div>";
+    }
 
     //list games
     html += "<div style=\"margin-left: 5%; margin-top: 15px\"><h1>Games</h1></div>";
-    for( i = 0; i < games.length && i < 5; i++){
-        //html += "<div style=\"margin-left: 10%; border-bottom-style: solid; border-width: 2px\"; onclick=\"window.location=\'/gameTitle/" + games[i]["id"] + "\'\"'><h1>" + games[i]["name"] + "</h1></div>";
-        html += "<div onmouseout=\"this.style.color=\'black\'\" onmouseover=\"this.style.color=\'#29dd00\'\" style=\"margin-left: 25%; margin-bottom: 2%; width: 50%; background-color: \'white\';\"; onclick=\"window.location=\'/gameTitle/"
-        html += games[i].id;
-        html += "\'\"'> <img height=\"50px\" src=\"./staticImages/gameIcon.png\" align=\"right\"><h3 style=\"font-family: Arial\">"
-        html += games[i].name;
-         if(games[i].total_rating != null){
-            html += "</h3> <font color=\"black\"> Average Score</font> : "+games[i].total_rating+"</font> </div>";
+    if(games != null && games.length != 0){
+        for( i = 0; i < games.length && i < 5; i++){
+            //html += "<div style=\"margin-left: 10%; border-bottom-style: solid; border-width: 2px\"; onclick=\"window.location=\'/gameTitle/" + games[i]["id"] + "\'\"'><h1>" + games[i]["name"] + "</h1></div>";
+            html += "<div onmouseout=\"this.style.color=\'black\'\" onmouseover=\"this.style.color=\'#29dd00\'\" style=\"margin-left: 25%; margin-bottom: 2%; width: 50%; background-color: \'white\';\"; onclick=\"window.location=\'/gameTitle/"
+            html += games[i].id;
+            html += "\'\"'> <img height=\"50px\" src=\"./staticImages/gameIcon.png\" align=\"right\"><h3 style=\"font-family: Arial\">"
+            html += games[i].name;
+             if(games[i].total_rating != null){
+                html += "</h3> <font color=\"black\"> Average Score</font> : "+games[i].total_rating+"</font> </div>";
+            }
+            else {
+                html += "</h3> <font color=\"black\"> Average Score</font> : No Rating</font> </div>";
+            }
         }
-        else {
-            html += "</h3> <font color=\"black\"> Average Score</font> : No Rating</font> </div>";
-        }
+    }
+    else {
+        html += "<div style=\"margin-left: 25%; margin-top: 2%\"><h2>No Results</h2></div>";
     }
 
     //list movies
     html += "<div style=\"margin-left: 5%; margin-top: 15px\"><h1>Movies</h1></div>";
-    for (i = 0; i < movies["total_results"] && i < 5; i++){
-	    //html += "<div style=\"margin-left: 10%; border-bottom-style: solid; border-width: 2px\"; onclick=\"window.location=\'/movie/" + movies["results"][i]["id"] + "\'\"'><h1>" + movies["results"][i]["title"] + "</h1></div>";
-        html += "<div onmouseout=\"this.style.color=\'black\'\" onmouseover=\"this.style.color=\'#4b6d93\'\" style=\"margin-left: 25%; margin-bottom: 2%; width: 50%; background-color: \'white\';\"; onclick=\"window.location=\'/movie/" 
-        html += movies["results"][i]["id"] 
-        html += "\'\"'> <img height=\"50px\" src=\"./staticImages/movieIcon.jpg\" align=\"right\"><h3 style=\"font-family: Arial\">" 
-        html += movies["results"][i]["title"] + "</h3> <font color=\"#dd4300\"> Average Score</font> : "+ movies["results"][i]["vote_average"] +"</font> </div>";
+    if(movies["results"] != null && movies["results"].length != 0){
+        for (i = 0; i < movies["total_results"] && i < 5; i++){
+    	    //html += "<div style=\"margin-left: 10%; border-bottom-style: solid; border-width: 2px\"; onclick=\"window.location=\'/movie/" + movies["results"][i]["id"] + "\'\"'><h1>" + movies["results"][i]["title"] + "</h1></div>";
+            html += "<div onmouseout=\"this.style.color=\'black\'\" onmouseover=\"this.style.color=\'#4b6d93\'\" style=\"margin-left: 25%; margin-bottom: 2%; width: 50%; background-color: \'white\';\"; onclick=\"window.location=\'/movie/" 
+            html += movies["results"][i]["id"] 
+            html += "\'\"'> <img height=\"50px\" src=\"./staticImages/movieIcon.jpg\" align=\"right\"><h3 style=\"font-family: Arial\">" 
+            html += movies["results"][i]["title"] + "</h3> <font color=\"#dd4300\"> Average Score</font> : "+ movies["results"][i]["vote_average"] +"</font> </div>";
+        }
+    }
+    else {
+        html += "<div style=\"margin-left: 25%; margin-top: 2%\"><h2>No Results</h2></div>";
     }
 
 
@@ -97,6 +111,9 @@ var populateHtml = function(){
                         html += books["GoodreadsResponse"]["search"]["results"]["work"][i]["best_book"]["title"]["_text"] + "</h3> <font color=\"#dd4300\"> Average Score</font> : "+ books["GoodreadsResponse"]["search"]["results"]["work"][i]["average_rating"]["_text"] +"</font> </div>";
                     }
         }
+    }
+    else {
+        html += "<div style=\"margin-left: 25%; margin-top: 2%\"><h2>No Results</h2></div>"
     }
     bodyDiv.innerHTML = html;
 };
